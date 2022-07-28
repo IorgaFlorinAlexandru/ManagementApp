badb5bc5075ace2d1e2c7b598824eadd2becab8d
IorgaFlorinAlexandru <iorgaflorinalexandru@gmail.com>
2022-07-28 11:52:39 +0300

    electron integration

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    mainWindow.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then( () => {
+    createWindow()
+
+})
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
+
